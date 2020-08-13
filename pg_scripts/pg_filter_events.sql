8cc9f6412e32a5d190683ca4c66a6700f2ef879f
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-08-14 00:22:04 +0800

    Filter non-null valuenums

--- pg_scripts/pg_filter_events.sql
+++ pg_scripts/pg_filter_events.sql
@@ -1,17 +1,16 @@
 select
-    labevents.subject_id,
-    labevents.hadm_id,
-    icustays.icustay_id,
-    labevents.itemid,
-    labevents.charttime,
-    labevents.value,
-    labevents.valuenum,
-    labevents.valueuom
-from labevents inner join icustays on 
-    labevents.subject_id = icustays.subject_id
-    and labevents.hadm_id = icustays.hadm_id
-    and labevents.charttime >= icustays.intime
-    and labevents.charttime <= icustays.outtime
+    l.subject_id,
+    l.hadm_id,
+    i.icustay_id,
+    l.itemid,
+    l.charttime,
+    l.valuenum,
+    l.valueuom
+from labevents l inner join icustays i on 
+    l.subject_id = i.subject_id
+    and l.hadm_id = i.hadm_id
+    and l.charttime >= i.intime
+    and l.charttime <= i.outtime
 where itemid in (
         50882, -- bicarbonate (mEq/L)
         50902, -- chloride
@@ -26,21 +25,21 @@ where itemid in (
         51300, 51301, -- white blood cells
         51081 -- serum creatinine
     )
-    and icustays.los >= 3
+    and i.los >= 3
+    and l.valuenum is not null
 
 union all
 
 select 
-    chartevents.subject_id,
-    chartevents.hadm_id,
-    chartevents.icustay_id,
-    chartevents.itemid,
-    chartevents.charttime,
-    chartevents.value,
-    chartevents.valuenum,
-    chartevents.valueuom
-from chartevents inner join icustays on 
-    chartevents.icustay_id = icustays.icustay_id
+    c.subject_id,
+    c.hadm_id,
+    c.icustay_id,
+    c.itemid,
+    c.charttime,
+    c.valuenum,
+    c.valueuom
+from chartevents c inner join icustays i on 
+    c.icustay_id = i.icustay_id
 where itemid IN (
         226707, -- height (inches)
         226730, -- height (cm)
@@ -48,4 +47,5 @@ where itemid IN (
         763, -- weight (kg)
         224639 -- weight (kg)
     )
-    and icustays.los >= 3
+    and i.los >= 3
+    and c.valuenum is not null
