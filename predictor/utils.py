73342af6a6664a72ba0ed2f4349adfee9760cf72
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-08-25 15:01:39 +0800

    Added evaluation script

--- predictor/utils.py
+++ predictor/utils.py
@@ -1,16 +1,37 @@
-import torch
+# import torch
+import numpy as np
 
 
-def get_mask_for(x):
-    # exclude day 1 and padding days
-    mask = x.bool().any(dim=-1)
-    mask[:, 0] = False
-    return mask
+# def get_mask_for(x):
+#     # exclude day 1 and padding days
+#     mask = x.bool().any(dim=-1)
+#     mask[:, 0] = False
+#     return mask
 
+
+# def convert_preds(x, y, y_hat):
+#     # get the indices of the last day of a specific ICU stay
+#     indices = x.bool().any(dim=-1).int().sum(dim=-1) - 1
+
+#     # create a new y and y_hat only considering the values
+#     # of the last day predictions
+#     _y = []
+#     _y_hat = []
+
+#     # TODO: somehow refactor this so that it doesn't use a for-loop
+#     for i in range(y.size(0)):
+#         last_day_index = indices[i]
+#         _y.append(y[i, last_day_index, 0])
+#         _y_hat.append(y_hat[i, last_day_index, 0])
+
+#     return (
+#         torch.tensor(_y, dtype=torch.float32),
+#         torch.tensor(_y_hat, dtype=torch.float32),
+#     )
 
 def convert_preds(x, y, y_hat):
     # get the indices of the last day of a specific ICU stay
-    indices = x.bool().any(dim=-1).int().sum(dim=-1) - 1
+    indices = np.any(x, axis=-1).sum(axis=-1) - 1
 
     # create a new y and y_hat only considering the values
     # of the last day predictions
@@ -18,12 +39,12 @@ def convert_preds(x, y, y_hat):
     _y_hat = []
 
     # TODO: somehow refactor this so that it doesn't use a for-loop
-    for i in range(y.size(0)):
+    for i in range(y.shape[0]):
         last_day_index = indices[i]
         _y.append(y[i, last_day_index, 0])
         _y_hat.append(y_hat[i, last_day_index, 0])
 
     return (
-        torch.tensor(_y, dtype=torch.float32),
-        torch.tensor(_y_hat, dtype=torch.float32),
+        np.array(_y).astype(np.float32),
+        np.array(_y_hat).astype(np.float32),
     )
---
+++ tf-evaluate.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+from predictor.models import TFAkiLstm, TFAkiGpt2
+from predictor.utils import convert_preds
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    roc_auc_score,
+)
+
+import fire
+import numpy as np
+import os
+import torch
+
+TIMESTEPS = 8
+N_FEATURES = 16
+
+
+def evaluate(
+    ckpt_dir: str = 'saved_models',
+    testing: str = 'matrix_testing.npy',
+    dataset_dir: str = 'dataset',
+):
+    '''
+    Tests the serialized models inside the `ckpt_dir` directory.
+    `ckpt_directory` should contain ONLY the model's weights and not the
+    entire model (saved using torch.save(model.state_dict())). Filenames
+    should follow the pattern {architecture}_e{epochs}_l{n_layers}.pt.
+
+    Parameters:
+    ckpt_dir: The location of the serialized model state dicts
+    testing: The testing data to be used (a numpy 3d-array serialized using np.save)
+    dataset_dir: The directory that contains the `testing` numpy array
+    '''
+    ckpt_dir = Path(ckpt_dir)
+    assert ckpt_dir.exists(), \
+        f'"{ckpt_dir}" is empty. Train models first.'
+
+    dataset_path = Path(dataset_dir)
+    testing_path = dataset_path / testing
+    assert testing_path.exists(), f'{testing} does not exist'
+
+    test_matrix = np.load(testing_path).astype(np.float32)
+    test_x = test_matrix[:, :, :-1]
+    test_y = test_matrix[:, :, -1:]
+
+    for model in get_models(ckpt_dir):
+        y_hat, _ = model(test_x)
+        y, y_hat = convert_preds(test_x, test_y, y_hat)
+
+        cm = confusion_matrix(y, np.around(y_hat))
+        acc = accuracy_score(y, np.around(y_hat))
+        score = roc_auc_score(y, y_hat)
+        report = classification_report(y, np.around(y_hat))
+
+        print(f'\n[INFO] Evaluation Results: {model.__class__.__name__}')
+        print(cm)
+        print(f'Accuracy: {acc:.4%}')
+        print(f'ROC AUC SCORE: {score:.4%}')
+        print(report)
+        print('=' * 40)
+
+
+def get_models(ckpt_dir: Path):
+    for dname in os.listdir(ckpt_dir):
+        # get the model's architecture
+        # from the filename of its trained weights
+        architecture, _ = dname.split('_')
+
+        # load model's trained weights
+        model_weights_path = ckpt_dir / dname / architecture
+
+        # create model and restore its trained weights
+        model = get_model(architecture)
+        model.load_weights(model_weights_path)
+        yield model
+
+
+def get_model(architecture: str):
+    if architecture == 'gpt2':
+        return TFAkiGpt2(
+            n_heads=2,
+            timesteps=TIMESTEPS,
+            n_features=N_FEATURES,
+            n_layers=1,
+        )
+
+    if architecture == 'lstm':
+        return TFAkiLstm(
+            timesteps=TIMESTEPS,
+            n_features=N_FEATURES,
+        )
+
+    raise AssertionError(f'Unknown architecture "{architecture}"')
+
+
+if __name__ == '__main__':
+    fire.Fire(evaluate)
