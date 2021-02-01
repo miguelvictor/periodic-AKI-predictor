Fixed incorrect masking on padding days

models.py
@@ -1,4 +1,5 @@
 from transformers.models.gpt2.modeling_gpt2 import GPT2Config, Block
+from typing import Optional
 
 import pytorch_lightning as pl
 import torch
@@ -25,7 +26,7 @@ class BaseModel(nn.Module):
         ])
         self.ln_f = nn.LayerNorm(N_FEATURES, eps=CONFIG.layer_norm_epsilon)
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor, attn_mask: Optional[torch.Tensor] = None):
         # input data's should be a 3D tensor
         assert x.ndim == 3
         _, n_timesteps, n_features = x.size()
@@ -36,10 +37,6 @@ class BaseModel(nn.Module):
             n_timesteps, dtype=torch.long, device=x.device)
         hidden_states = x + self.wpe(position_ids)
         hidden_states = self.drop(hidden_states)
-
-        # create attention mask
-        attn_mask = x.bool().any(dim=-1).float()[:, None, None, :]
-        attn_mask = (1.0 - attn_mask) * -10000.0
 
         # go through layer blocks
         for block in self.h:
@@ -73,10 +70,24 @@ class PredictiveModel1(pl.LightningModule):
         self.head = nn.Linear(N_FEATURES, N_FEATURES)
         self.drop = nn.Dropout(CONFIG.resid_pdrop)
 
-    def forward(self, x: torch.Tensor):
-        hidden_states = self.base(x)
+    def forward(self, x: torch.Tensor, attn_mask: Optional[torch.Tensor] = None):
+        # retrieve batch size from input tensor
+        assert x.ndim == 3
+        batch_size, timesteps, _ = x.shape
+
+        # retrieve by going through GPT-2 body
+        hidden_states = self.base(x, attn_mask=attn_mask)
+
+        # project hidden states
         output_values = self.head(hidden_states)
         output_values = self.drop(output_values)
+
+        # also apply attention mask for the head layer
+        if attn_mask is not None:
+            attn_mask = (~attn_mask.bool()).float()
+            attn_mask = attn_mask.view(batch_size, timesteps, 1)
+            output_values = output_values * attn_mask
+
         return output_values
 
     def training_step(self, batch, _):
@@ -88,11 +99,15 @@ class PredictiveModel1(pl.LightningModule):
         x, y = batch[:, :-1], batch[:, 1:]
 
         # create attention mask to exclude padded days
-        mask = x.bool().any(dim=-1)
+        # note that the attention mask should be based on the labels y
+        # since inputs x may contain data without the necessary label y
+        attn_mask = y.bool().any(dim=-1).float()
+        attn_mask = (1 - attn_mask) * -10000.0
+        attn_mask = attn_mask[:, None, None, :]
 
         # do forward pass and compute loss
-        y_hat = self(x)
-        loss = F.mse_loss(y_hat[mask], y[mask])
+        y_hat = self(x, attn_mask=attn_mask)
+        loss = F.mse_loss(y_hat, y)
         self.log('train_loss', loss)
         return loss
 

prepare-pretraining-data.py
@@ -336,13 +336,13 @@ def transform_to_tensor(input_path, output_path):
     n_stays_before = len(pd.unique(df['stay_id']))
     df = df.groupby('stay_id').apply(padding).reset_index(drop=True)
     n_stays_after = len(pd.unique(df['stay_id']))
-    n_stays, remainder = divmod(df.shape[0], TIMESTEPS)
+    n_stays, remainder = divmod(df.shape[0], TIMESTEPS + 1)
     assert n_stays_before == n_stays_after
     assert n_stays == n_stays_after
     assert remainder == 0
 
     # reshape tensor into a 3D tensor with shape [_, timesteps, n_features]
-    matrix = df[columns].values.reshape(-1, TIMESTEPS, N_FEATURES)
+    matrix = df[columns].values.reshape(-1, TIMESTEPS + 1, N_FEATURES)
     np.save(output_path, matrix)
     logger.info('`transform_outliers` has ended')
 
@@ -353,9 +353,9 @@ def padding(group) -> pd.DataFrame:
     a number of rows equal to the timesteps constant. Also, stay_id
     column is preserved on the padding rows.
     '''
-    group = group[:TIMESTEPS]
+    group = group[:TIMESTEPS+1]
     n_rows, n_cols = group.shape
-    n_rows = TIMESTEPS - n_rows
+    n_rows = TIMESTEPS + 1 - n_rows
     padding = np.zeros((n_rows, n_cols))
     padding = pd.DataFrame(padding, columns=group.columns)
     padding['stay_id'] = group['stay_id'].iloc[0]